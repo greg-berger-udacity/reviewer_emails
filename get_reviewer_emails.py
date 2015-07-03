b66bfc399391523101c72798e784aaae862358aa
Surajit A Bose <surajit@udacity.com>
2015-07-03 00:41:23 -0700

    Instead of list of emails, create a dictionary with emails as keys and reviewer first names as values. Write out emails and names to file.

--- get_reviewer_emails.py
+++ get_reviewer_emails.py
@@ -1,24 +1,20 @@
-# Script for generating a list of grader email addresses
+# Script for generating a list of grader email addresses and first names
 
 # Download CSV of graders from grading tool rails admin
 # Name this CSV 'names.csv'; output will be in 'names2.csv'
 
 import csv
 
-graders = []
-emails = []
+emails = {}
 with open ('names.csv', 'r') as csvfile:
-    csv_rows = csv.reader(csvfile)
-    
-    # Assumes that reviewer's email is in the second column
-    for row in csv_rows:
-        if row[1] not in emails:
-            graders.append(row)
-            emails.append(row[1])
+   csv_rows = csv.reader(csvfile)
+   
+   # Assumes that name is in the first column
+   # and email address is in the second column
+   for row in csv_rows:
+       emails[row[1]] = row[0]
 
+# Writes name and email addresses out into csv
 with open ('names2.csv', 'w') as writefile:
-    written_file = csv.writer(writefile)
-    for grader in graders:
-        written_file.writerow(grader)
-
-
+   for (key, value) in emails.iteritems():
+       writefile.write(value + ',' + key + '\r')
